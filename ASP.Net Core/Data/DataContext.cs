8dada78a011a1f6497138ed8e0f07caf8c1d2c74
Abubakarbaiggg <Abubakar192005@gmail.com>
2026-05-02 21:43:46 +0500

    Courses Seeder.

--- ASP.Net Core/Data/DataContext.cs
+++ ASP.Net Core/Data/DataContext.cs
@@ -1,7 +1,8 @@
 ﻿using ASP.Net_Core.Models;
+using ASP.Net_Core.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using ASP.Net_Core.ViewModels;
 
 namespace ASP.Net_Core.Data
 {
@@ -17,5 +18,14 @@ namespace ASP.Net_Core.Data
         public DbSet<StudentCourse> StudentCourses { get; set; }
         public DbSet<ASP.Net_Core.ViewModels.RoleStore> RoleStore { get; set; } = default!;
         public DbSet<ASP.Net_Core.ViewModels.AppUserViewModel> AppUserViewModel { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Course>().HasData(
+                new Course { Id = 4, Title = "Business Administration", Code = "BA" },
+                new Course { Id = 5, Title = "Urdu Literature", Code = "URD" },
+                new Course { Id = 6, Title = "Islamic Studies", Code = "ISL" }
+            );
+        }
     }
 }
---
+++ ASP.Net Core/Migrations/20260502164130_courseSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ASP.Net_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class courseSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "Code", "Title" },
+                values: new object[,]
+                {
+                    { 4, "BA", "Business Administration" },
+                    { 5, "URD", "Urdu Literature" },
+                    { 6, "ISL", "Islamic Studies" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
