4081349b21d1779e789b2e19c48f372f9b08ef0b
Abubakarbaiggg <Abubakar192005@gmail.com>
2026-05-02 18:40:41 +0500

    Student Validation Add.

---
+++ ASP.Net Core/Migrations/20260502133223_studentValidation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.Net_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class studentValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+    }
+}
--- ASP.Net Core/Models/Student.cs
+++ ASP.Net Core/Models/Student.cs
@@ -1,11 +1,19 @@
-﻿namespace ASP.Net_Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.Net_Core.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Full Name")]
+        [MaxLength(255,ErrorMessage ="Name is required field")]
         public string Name { get; set; }
+        [Required]
         public DateTime Enrolled { get; set; }
         public ICollection<StudentCourse> Enrollment { get; set; }
+        [Required]
         public String ProfileImage { get; set; }
     }
 }
